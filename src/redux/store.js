422df45d686e9b9756180818fea5a2c80d415d3c
ggomma <ggomma@gmail.com>
2018-06-30 18:27:46 +0900

    Add redux middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import reducers from './modules';
 
+
+const logger = createLogger();
 
 const store = createStore(
   reducers,
   /* eslint no-underscore-dangle: 0 */
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, thunk),
 );
 
 export default store;

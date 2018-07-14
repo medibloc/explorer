b00602e34174862bc49cfe25e908eab776e49222
ggomma <ggomma@gmail.com>
2018-07-14 16:39:08 +0900

    Styling footer

--- src/container/Footer/Footer.js
+++ src/container/Footer/Footer.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import './Footer.scss';
+
+const sns = ['telegram', 'twitter', 'fb', 'blog', 'medi'];
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="footer">
+    <div className="footerContent">
+      <div className="footerLogo">
+        <img src="/image/icon/logo-footer.svg" alt="footerLogo" />
+      </div>
+      <div className="footerRights">
+        ©2018 MediBloc. All Rights Reserved.
+      </div>
+      <div className="footerSNS">
+        {
+          sns.map(service => (
+            <img src={`/image/icon/ico-${service}-on@3x.png`} alt="sns" />
+          ))
+        }
+      </div>
+    </div>
   </div>
 );
 
-export default Footer;
+const mapStateToProps = () => ({
+
+});
+
+export default connect(mapStateToProps)(Footer);

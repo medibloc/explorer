6c6945e19966f5e5e6607ebd65acc2b31f614b27
ggomma <ggomma@gmail.com>
2018-07-16 18:04:22 +0900

    Restyle searchbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
+
+import Layout from './Layout';
 import Blocks from '../container/Blocks';
 import Txs from '../container/Txs/Txs';
-
 import SearchBar from '../container/SearchBar';
 import LiveInfoWrapper from '../components/LiveInfoWrapper';
 
@@ -10,48 +11,50 @@ import './pages.scss';
 
 
 const Home = () => (
-  <div className="home">
-    <div className="homeContentWrapper">
-      <div className="homeTokenInfo">
-        <div>
-          <span className="homeTokenInfoTitle">
-            Market Cap
-          </span>
-          <div className="homeInfoValue">
-            <span>
-              123,456,789
+  <Layout>
+    <div className="home">
+      <div className="homeContentWrapper">
+        <div className="homeTokenInfo">
+          <div>
+            <span className="homeTokenInfoTitle">
+              Market Cap
             </span>
-            USD
+            <div className="homeInfoValue">
+              <span>
+                123,456,789
+              </span>
+              USD
+            </div>
+          </div>
+          <div className="verticalLine" />
+          <div>
+            <span className="homeTokenInfoTitle">
+              Med Supply
+            </span>
+            <div className="homeInfoValue">
+              <span>
+                100,000,000
+              </span>
+              MED
+            </div>
           </div>
         </div>
-        <div className="verticalLine" />
-        <div>
-          <span className="homeTokenInfoTitle">
-            Med Supply
-          </span>
-          <div className="homeInfoValue">
-            <span>
-              100,000,000
-            </span>
-            MED
-          </div>
+        <div className="homeSearchBar">
+          <hr />
+          <SearchBar type="main" />
         </div>
-      </div>
-      <div className="homeSearchBar">
-        <hr />
-        <SearchBar type="main" />
-      </div>
-      <div className="homeLive">
-        <LiveInfoWrapper title="Recent Block">
-          <Blocks />
-        </LiveInfoWrapper>
-        <div className="verticalLine" />
-        <LiveInfoWrapper title="Recent Transaction">
-          <Txs />
-        </LiveInfoWrapper>
+        <div className="homeLive">
+          <LiveInfoWrapper title="Recent Block">
+            <Blocks />
+          </LiveInfoWrapper>
+          <div className="verticalLine" />
+          <LiveInfoWrapper title="Recent Transaction">
+            <Txs />
+          </LiveInfoWrapper>
+        </div>
       </div>
     </div>
-  </div>
+  </Layout>
 );
 
 export default Home;

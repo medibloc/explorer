cdb77dd430198a4da45466704d674021ce0ecc2d
Heekyu Kim <heekyu83@gmail.com>
2018-07-23 22:38:33 +0900

    customize webpack - add sass-loader in start/build script

---
+++ config/config-overrides.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+module.exports = (config) => {
+  // Add the SASS loader second-to-last
+  // (last one must remain as the "file-loader")
+  let loaderList = config.module.rules[1].oneOf;
+  loaderList.splice(loaderList.length - 1, 0, {
+    test: /\.scss$/,
+    use: ["style-loader", "css-loader", "sass-loader"]
+  });
+};

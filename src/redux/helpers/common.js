c7647fec5bae1cf9413c21e9159d4a86ed966cdc
ggomma <ggomma@gmail.com>
2018-07-26 11:29:34 +0900

    Remove promise in simpleREquester axios

--- src/redux/helpers/common.js
+++ src/redux/helpers/common.js
@@ -1,28 +1,27 @@
 import axios from 'axios';
 
+
 // eslint-disable-next-line
 export const simpleRequester = (dispatch, {
   url,
   params = null,
   actionType,
   ERROR,
-}) => new Promise((resolve, reject) => (
-  axios({
-    url,
-    params,
+}) => axios({
+  url,
+  params,
+})
+  .then((res) => {
+    dispatch({
+      type: actionType,
+      payload: res.data,
+    });
+    return res.data;
   })
-    .then((res) => {
-      dispatch({
-        type: actionType,
-        payload: res.data,
-      });
-      return resolve(res.data);
-    })
-    .catch((err) => {
-      dispatch({
-        type: ERROR,
-        payload: err.message,
-      });
-      return reject(err.message);
-    })
-));
+  .catch((err) => {
+    dispatch({
+      type: ERROR,
+      payload: err.message,
+    });
+    throw err.message;
+  });

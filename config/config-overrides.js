f36c9bf7d17bd89088407b9af7707e121932cce1
minho.yoo <minho.yoo.dev@gmail.com>
2018-08-01 10:12:23 +0900

    add postcss-loader for scss config

--- config/config-overrides.js
+++ config/config-overrides.js
@@ -1,10 +1,21 @@
 /* eslint-disable */
 module.exports = (config) => {
-  // Add the SASS loader second-to-last
-  // (last one must remain as the "file-loader")
-  let loaderList = config.module.rules[1].oneOf;
-  loaderList.splice(loaderList.length - 1, 0, {
-    test: /\.scss$/,
-    use: ["style-loader", "css-loader", "sass-loader"]
-  });
+	// Add the SASS loader second-to-last
+	// (last one must remain as the "file-loader")
+
+	// Added postcss-loader before sass-loader
+	// postcss is post processing css (https://github.com/postcss/postcss-loader)
+
+	let loaderList = config.module.rules[1].oneOf;
+	loaderList.splice(loaderList.length - 1, 0,
+		{
+			test: /\.scss$/,
+			use: [
+				"style-loader",
+				{ loader: 'css-loader', options: { importLoaders: 1 } },
+				'postcss-loader',
+				"sass-loader"
+			]
+		}
+	);
 };

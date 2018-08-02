f64ce0447d7eda27353c213fdf7d53288a7c664e
minho.yoo <minho.yoo.dev@gmail.com>
2018-08-02 17:57:27 +0900

    Fix postcss config

--- config/config-overrides.js
+++ config/config-overrides.js
@@ -13,7 +13,26 @@ module.exports = (config) => {
 			use: [
 				"style-loader",
 				{ loader: 'css-loader', options: { importLoaders: 1 } },
-				'postcss-loader',
+				{
+					loader: require.resolve('postcss-loader'),
+					options: {
+						// Necessary for external CSS imports to work
+						// https://github.com/facebookincubator/create-react-app/issues/2677
+						ident: 'postcss',
+						plugins: () => [
+							require('postcss-flexbugs-fixes'),
+							require('autoprefixer')({
+								browsers: [
+									'>1%',
+									'last 4 versions',
+									'Firefox ESR',
+									'not ie < 9', // React doesn't support IE8 anyway
+								],
+								flexbox: 'no-2009',
+							}),
+						],
+					},
+				},
 				"sass-loader"
 			]
 		}

0956d450be6e33464fa1bfed57f2a7954cee0468
Heekyu Kim <heekyu83@gmail.com>
2018-08-27 20:15:20 +0900

    server - send numCandidate

--- server/src/infoController.js
+++ server/src/infoController.js
@@ -1,10 +1,23 @@
+import axios from 'axios';
+
+import config from '../config';
+
 import Account from './account/model';
 import Block from './block/model';
 import Transaction from './transaction/model';
+
+const { url } = config.blockchain;
 
 export default async (req, res) => {
   const numAccount = await Account.count();
   const { height } = await Block.findOne({ order: [['id', 'DESC']] });
   const numTx = await Transaction.count();
-  res.json({ height, numAccount, numTx });
+  const { data: { candidates } } = await axios({
+    method: 'get',
+    url: `${url}/v1/candidates`,
+  });
+
+  res.json({
+    height, numAccount, numCandidate: candidates.length, numTx,
+  });
 };

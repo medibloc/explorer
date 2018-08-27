6893d30cc9db39e26f8d0dee6b2fc2ce2e68c026
Heekyu Kim <heekyu83@gmail.com>
2018-08-27 20:31:04 +0900

    server - update account model

--- server/src/account/model.js
+++ server/src/account/model.js
@@ -5,7 +5,7 @@ import { data, id } from '../db/columns';
 
 export default db.define('accounts', {
   address: { type: Sequelize.STRING, unique: true },
-  balance: { defaultValue: '0', type: Sequelize.DECIMAL(16, 8) },
+  balance: { defaultValue: '0', type: Sequelize.DECIMAL(24, 8) },
   data, // last data received from node(BP)
   id,
 });

63189519dafcc94733909bbc11d62fc2b95f8c7d
Heekyu Kim <heekyu83@gmail.com>
2018-08-28 20:55:13 +0900

    server - send tx data in account txs_from, txs_to

--- server/src/account/controller.js
+++ server/src/account/controller.js
@@ -1,9 +1,44 @@
+import { keyBy } from 'lodash';
+import { Op } from 'sequelize';
+
 import Account from './model';
+import Transaction from '../transaction/model';
 import { listQueryWithCount } from '../db/query';
 
 export const get = async (req, res) => {
-  const account = await Account.findById(req.params.id);
-  res.json({ account });
+  const { id } = req.params;
+  let account;
+  if (+id) {
+    account = await Account.findById(req.params.id);
+  } else {
+    account = await Account.findOne({ where: { address: id } });
+  }
+  // TODO use accountLogs
+
+  const { data: { txs_from: txsFrom, txs_to: txsTo } } = account;
+  const txs = {};
+  [...txsFrom, ...txsTo].forEach((tx) => {
+    txs[tx] = {};
+  });
+  const dbTxs = await Transaction.findAll({ where: { txHash: { [Op.in]: Object.keys(txs) } } });
+  const dbTxsMap = keyBy(dbTxs, 'txHash');
+  txsFrom.forEach((tx, i) => {
+    txsFrom[i] = dbTxsMap[tx].dataValues;
+  });
+  txsTo.forEach((tx, i) => {
+    txsTo[i] = dbTxsMap[tx].dataValues;
+  });
+
+  res.json({
+    account: {
+      ...account.dataValues,
+      data: {
+        ...account.data,
+        txs_from: txsFrom,
+        txs_to: txsTo,
+      },
+    },
+  });
 };
 
 const searchColumns = [Account.tableAttributes.address];

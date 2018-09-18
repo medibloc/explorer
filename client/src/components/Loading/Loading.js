836a9f247035a05bcb630fdb7ace8f8a8cd7f461
ggomma <ggomma@gmail.com>
2018-09-18 17:28:39 +0900

    Change loading page

--- client/src/components/Loading/Loading.js
+++ client/src/components/Loading/Loading.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactLoading from 'react-loading';
 
 
+import './loading.scss';
+
 const Loading = () => (
-  <ReactLoading
-    type="spokes"
-    color="#397ff4"
-    height={40}
-    width={40}
-  />
+  <div className="loading">
+    <ReactLoading
+      type="spokes"
+      color="#397ff4"
+      height={40}
+      width={40}
+    />
+  </div>
 );
 
 export default Loading;

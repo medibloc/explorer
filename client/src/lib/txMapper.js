4e7e1dfbaf6c8a4461518eda295ea6fdbdcc4cf2
ggomma <ggomma@gmail.com>
2018-10-05 08:57:28 +0900

    Distinguish transaction payload with tx type

--- client/src/lib/txMapper.js
+++ client/src/lib/txMapper.js
@@ -1,15 +1,43 @@
 import { local } from 'medjs';
 import { divider } from './bigNumCalculator';
+import { txTypes } from '../config';
 
-const { recoverDefaultPayload } = local.transaction;
+
+const {
+  recoverAddCertificationPayload,
+  recoverDataPayload,
+  recoverDefaultPayload,
+  recoverRevokeCertificationPayload,
+  recoverVotePayload,
+} = local.transaction;
 
 const txMapper = (tx) => {
   const tempAmount = divider(tx.value, [10 ** 12]).split('.');
   tempAmount[0] = tempAmount[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   const Amount = `${tempAmount.join('.')} MED`;
 
-  let payload = ''
-  if (tx.payload !== '' && tx.payload !== undefined) payload = recoverDefaultPayload(tx.payload);
+  let payload = '';
+  if (tx.payload !== '' && tx.payload !== undefined) {
+    switch (tx.tx_type) {
+      case txTypes.ADD_CERTIFICATION:
+        payload = recoverAddCertificationPayload(tx.payload);
+        break;
+      case txTypes.DATA_UPLOAD:
+        payload = recoverDataPayload(tx.payload);
+        break;
+      case txTypes.REVOKE_CERTIFICATION:
+        payload = recoverRevokeCertificationPayload(tx.payload);
+        break;
+      case txTypes.VALUE_TRANSFER:
+        payload = recoverDefaultPayload(tx.payload);
+        break;
+      case txTypes.VOTE:
+        payload = recoverVotePayload(tx.payload);
+        break;
+      default:
+        break;
+    }
+  }
 
   return {
     'Transaction Hash': tx.hash,

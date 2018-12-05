7cdac1d4346c6229eb09ed4b883becf3e0c5b2b0
ggomma <ggomma@gmail.com>
2018-12-06 08:35:42 +0900

    Seperate parser from blockchain/handler.js

---
+++ server/src/utils/parser.js
@@ -0,0 +1,31 @@
+const parseBlock = block => ({
+  data: block,
+  hash: block.hash,
+  height: +block.height,
+  id: +block.height,
+});
+
+const parseTx = (block, tx) => ({
+  blockHeight: block.height,
+  blockId: block.id,
+  data: tx,
+  executed: tx.receipt ? tx.receipt.executed : false,
+  fromAccount: tx.from,
+  onChain: tx.on_chain,
+  toAccount: tx.to,
+  txHash: tx.hash,
+});
+
+const parseAccount = account => ({
+  alias: account.alias || null,
+  balance: account.balance,
+  candidateId: account.candidate_id || null,
+  data: account,
+  vesting: account.vesting,
+});
+
+export {
+  parseBlock,
+  parseTx,
+  parseAccount,
+};

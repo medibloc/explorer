394a637502a6fadd8611961b23c8d0b742465a07
ggomma <ggomma@gmail.com>
2018-12-06 19:42:31 +0900

    Fix account staking calculation

--- client/src/lib/accountMapper.js
+++ client/src/lib/accountMapper.js
@@ -1,16 +1,18 @@
 import { divider } from './bigNumCalculator';
 
 const accountMapper = (account, totalSupply = undefined) => {
-  const tempAmount = divider(account.balance, [10 ** 12]).split('.');
+  const tempAmount = divider(account.totalAmount, [10 ** 12]).split('.');
   tempAmount[0] = tempAmount[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   const Balance = `${tempAmount.join('.')} MED`;
+
+  const Staking = account.data ? `${divider(account.data.vesting, [10 ** 12]).replace(/\B(?=(\d{3})+(?!\d))/g, ',')} MED` : null;
 
   return {
     Account: account.address,
     Balance,
     Transactions: account.totalTxs,
     Percentage: totalSupply ? `${divider(account.balance, [totalSupply, 10 ** 12 / 100], 5)}` : 0,
-    Staking: `${divider(account.vesting, [10 ** 12]).replace(/\B(?=(\d{3})+(?!\d))/g, ',')} MED`,
+    Staking,
   };
 };
 

ccdfe38630d58c1c5365b69b04305cff457d9181
ggomma <ggomma@gmail.com>
2019-05-02 15:41:02 +0900

    Separate DetailWrapperValue component from DetailWrapper component

--- client/src/components/DetailWrapper/DetailWrapper.js
+++ client/src/components/DetailWrapper/DetailWrapper.js
@@ -24,6 +24,44 @@ const DetailWrapperKey = ({ titleList }) => (
   </div>
 );
 
+const DetailWrapperValue = ({
+  titleList, linkList, data, lang,
+}) => (
+  <div className="detailWrapperValue">
+    {
+      titleList.map((title) => {
+        for (let i = 0; i < linkList.length; i += 1) {
+          if (linkList[i].includes(title)) {
+            const linkTo = `/${lang}/${linkList[i].split('/')[0]}/${data[title]}`;
+
+            return (
+              <span key={title}>
+                {
+                  ['From', 'To'].includes(title) && (<QrButton modalData={data[title]} />)
+                }
+                {
+                  title === 'url'
+                    ? (<a href={data[title]}>{ data[title] }</a>)
+                    : (<NavLink to={linkTo}>{ data[title] }</NavLink>)
+                }
+              </span>
+            );
+          }
+        }
+        return (
+          <span key={title} className={cx({ message: title === 'Message' })}>
+            {
+              title === 'Time Stamp'
+                ? timezoneMatcher(data[title])
+                : data[title]
+            }
+          </span>
+        );
+      })
+    }
+  </div>
+);
+
 const DetailWrapper = ({
   data, lang, mode, type,
 }) => {
@@ -33,54 +71,20 @@ const DetailWrapper = ({
   return (
     <div className={cx('detailWrapper', { mobile: mode === 2 })}>
       <DetailWrapperKey titleList={titleList} />
-      <div className="detailWrapperValue">
-        {
-          titleList.map((title) => {
-            for (let i = 0; i < linkList.length; i += 1) {
-              if (linkList[i].includes(title)) {
-                return (
-                  <span key={title}>
-                    {
-                      (['From', 'To'].includes(title)) && (
-                        <QrButton modalData={data[title]} />
-                      )
-                    }
-                    {
-                      title === 'url'
-                        ? (
-                          <a href={data[title]}>
-                            { data[title] }
-                          </a>
-                        ) : (
-                          <NavLink to={`/${lang}/${linkList[i].split('/')[0]}/${data[title]}`}>
-                            { data[title] }
-                          </NavLink>
-                        )
-                    }
-                  </span>
-                );
-              }
-            }
-            return (
-              <span key={title} className={cx({ message: title === 'Message' })}>
-                {
-                  title === 'Time Stamp' ? (
-                    timezoneMatcher(data[title])
-                  ) : (
-                    data[title]
-                  )
-                }
-              </span>
-            );
-          })
-        }
-      </div>
+      <DetailWrapperValue titleList={titleList} linkList={linkList} data={data} lang={lang} />
     </div>
   );
 };
 
 DetailWrapperKey.propTypes = {
   titleList: PropTypes.array.isRequired,
+};
+
+DetailWrapperValue.propTypes = {
+  titleList: PropTypes.array.isRequired,
+  linkList: PropTypes.array.isRequired,
+  data: PropTypes.object.isRequired,
+  lang: PropTypes.string.isRequired,
 };
 
 DetailWrapper.propTypes = {

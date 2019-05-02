60243a20af68b90c68c839fcf3d856f1e47b2bed
ggomma <ggomma@gmail.com>
2019-05-02 20:10:34 +0900

    Set getter and setter for data because AWS RDS supports only mysql 5.6 for serverless which does not allow json type

--- server/src/db/columns.js
+++ server/src/db/columns.js
@@ -1,7 +1,15 @@
 import { singular } from 'pluralize';
 import Sequelize from 'sequelize';
 
-export const data = { type: Sequelize.JSON };
+export const data = {
+  type: Sequelize.TEXT('long'),
+  get: function () {
+    return JSON.parse(this.dataValues.data);
+  },
+  set: function (value) {
+    this.setDataValue('data', JSON.stringify(value));
+  },
+};
 export const id = { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true };
 export const refer = (parent, options = {}) => ({
   [`${singular(parent.name)}Id`]: {

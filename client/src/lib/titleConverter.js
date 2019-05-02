aca737e5bb9cdb743d2e2bacfbd1826e189f8146
ggomma <ggomma@gmail.com>
2019-05-02 15:09:46 +0900

    Use proper regular expression for titleConverter

--- client/src/lib/titleConverter.js
+++ client/src/lib/titleConverter.js
@@ -1,6 +1,6 @@
 const titleConverter = (title) => {
-  let convertedTitle = title.split(/ |\./)[0].toLowerCase();
-  if (title.split(/ |\./)[1]) convertedTitle += title.split(/ |\./)[1];
+  let convertedTitle = title.split(/[ .]/)[0].toLowerCase();
+  if (title.split(/[ .]/)[1]) convertedTitle += title.split(/[ .]/)[1];
   return convertedTitle;
 };
 

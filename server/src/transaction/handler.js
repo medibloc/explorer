81db9d5cdb8ce1fa0c3ac5e02d38ac1ad9820f92
ggomma <ggomma@gmail.com>
2019-07-21 18:07:09 +0900

    Remove unused functions in transaction handler

--- server/src/transaction/handler.js
+++ server/src/transaction/handler.js
@@ -29,15 +29,3 @@ export const handleTxsInDbBlock = async (dbBlock, t) => {
       return dbTxs;
     });
 };
-
-export const getTransactionsWithBlockHeight = (blockHeight, t) => (
-  Transaction.findAll({
-    where: { blockHeight },
-    transaction: t,
-  }));
-
-export const removeTransactionsWithBlockHeight = (blockHeight, t) => (
-  Transaction.destroy({
-    where: { blockHeight },
-    transaction: t,
-  }));

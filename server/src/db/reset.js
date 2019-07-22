35373efef42e29877d123f329c2d1b3972ad41d1
ggomma <ggomma@gmail.com>
2019-07-22 13:20:28 +0900

    Handle Candidate model in sync, reset process

--- server/src/db/reset.js
+++ server/src/db/reset.js
@@ -3,8 +3,9 @@ import { close } from './index';
 import Account from '../account/model';
 import Block from '../block/model';
 import Transaction from '../transaction/model';
+import Candidate from '../candidate/model';
 
-const reset = () => [Transaction, Account, Block].reduce(
+const reset = () => [Transaction, Candidate, Account, Block].reduce(
   (promise, model) => promise.then(() => model.destroy({
     where: {},
     // truncate: true,
--- server/src/db/sync.js
+++ server/src/db/sync.js
@@ -3,8 +3,9 @@ import { close } from './index';
 import Account from '../account/model';
 import Block from '../block/model';
 import Transaction from '../transaction/model';
+import Candidate from '../candidate/model';
 
-const sync = () => [Account, Block, Transaction].reduce(
+const sync = () => [Candidate, Account, Block, Transaction].reduce(
   (promise, model) => promise.then(() => model.sync({ alter: true })),
   Promise.resolve(),
 ).then(close);

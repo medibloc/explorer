7959358f6f9669413856f120e43478deb36e3a9b
ggomma <ggomma@gmail.com>
2019-07-22 13:17:51 +0900

    Add candidate controller

--- server/src/candidate/controller.js
+++ server/src/candidate/controller.js
@@ -1,50 +1,28 @@
-import BigNumber from 'bignumber.js';
 import { BadRequest } from 'http-errors';
-import { keyBy } from 'lodash';
-import { Op } from 'sequelize';
 
-import Account from '../account/model';
-import { toPagination } from '../db/query';
-
-import { requestCandidate, requestCandidates } from '../utils/requester';
+import Candidate from './model';
+import { listQueryWithCount } from '../db/query';
 
 export const get = async (req, res) => {
   const { id } = req.params;
-  if (id.length !== 64) {
-    throw new BadRequest('candidate id is not valid');
+  let candidate;
+  if (+id) {
+    candidate = await Candidate.findByPk(req.params.id);
+  }
+  if (!candidate) {
+    candidate = await Candidate.findOne({ where: { address: id } });
+  }
+  if (!candidate) {
+    throw new BadRequest('candidate not found');
   }
 
-  const candidate = await requestCandidate(id);
   res.json({ candidate });
 };
 
-export const list = async (req, res) => { // eslint-disable-line import/prefer-default-export
-  let candidates = await requestCandidates();
-  candidates.sort((c1, c2) => {
-    const diff = new BigNumber(c1.vote_power).minus(new BigNumber(c2.vote_power));
-    if (diff.isNegative()) return 1;
-    if (diff.isPositive()) return -1;
-    return 0;
-  });
-  const { offset, limit } = toPagination(req.query);
-  candidates = candidates.slice(offset, offset + limit);
-  if (!candidates.length) {
-    res.json({ candidates });
-    return;
-  }
+const searchColumns = [Candidate.tableAttributes.address];
 
-  const candidateIds = candidates.reduce(
-    (accumulator, candidate) => [...accumulator, candidate.candidate_id],
-    [],
-  );
-
-  const accounts = await Account.findAll({
-    where: { candidateId: { [Op.in]: candidateIds } },
-  });
-  const accountMap = keyBy(accounts, 'candidateId');
-  candidates.forEach((candidate) => {
-    // eslint-disable-next-line no-param-reassign
-    candidate.alias = accountMap[candidate.candidate_id].alias;
-  });
-  res.json({ candidates });
+export const list = async (req, res) => {
+  const options = { ...req.query, order: [['votes', 'DESC'], ['id', 'DESC']] };
+  const { data, pagination } = await listQueryWithCount(Candidate, options, searchColumns);
+  res.json({ candidates: data, pagination });
 };

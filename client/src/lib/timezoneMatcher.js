3bf308a1e1ce60242fa87faa873d08c888bb2585
ggomma <ggomma@gmail.com>
2019-07-22 10:51:35 +0900

    Update timezoneMatcher for UTC input

--- client/src/lib/timezoneMatcher.js
+++ client/src/lib/timezoneMatcher.js
@@ -1,9 +1,7 @@
-const timezoneMatcher = (unixTime) => {
-  // eslint-disable-next-line no-param-reassign
-  if (unixTime.toString().length === 13) unixTime = Math.floor(unixTime / 1000);
+const timezoneMatcher = (utc) => {
   const offset = new Date().getTimezoneOffset();
 
-  let date = new Date(unixTime * 1000 - offset * 60 * 1000);
+  let date = new Date(utc);
   date = date.toISOString().replace(/T/g, ' ').replace(/\..*/g, '');
   date += ` (UTC+${Math.floor(-offset / 60)})`;
 

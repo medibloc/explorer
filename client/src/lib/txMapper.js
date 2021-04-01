1a4ab4e6c5c62b328ce65b674c3ef612d0b5f46a
Youngjoon Lee <yjlee@medibloc.org>
2021-04-01 12:14:53 +0900

    feat: Display Tx Msg ID (instead of the suffix of TxHash)

--- client/src/lib/txMapper.js
+++ client/src/lib/txMapper.js
@@ -4,16 +4,24 @@ const txMapper = (tx) => {
   const tempAmount = divider(tx.amount, [10 ** 6], 6).split('.');
   tempAmount[0] = tempAmount[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   const Amount = `${tempAmount.join('.')} MED`;
+  let { txHash } = tx;
+  let msgId = '0';
+  if (new RegExp(':\\d+$').test(tx.txHash)) {
+    const colonIndex = tx.txHash.lastIndexOf(':');
+    txHash = tx.txHash.slice(0, colonIndex);
+    msgId = tx.txHash.slice(colonIndex + 1);
+  }
 
   return {
     'Block Height': tx.blockHeight,
-    'Transaction Hash': tx.txHash,
+    'Transaction Hash': txHash,
+    'Transaction MsgId': msgId,
     Status: tx.executed ? 'Success' : 'Fail',
     From: tx.fromAccount,
     To: tx.toAccount,
     Type: tx.type ? tx.type.split('/')[1] : '',
     Amount,
-    Message: tx.memo,
+    Memo: tx.memo,
     // Nonce: tx.nonce,
     // Signature: tx.sign,
   };
